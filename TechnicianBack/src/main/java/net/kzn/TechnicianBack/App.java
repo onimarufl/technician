46fb06ac17c4b8ec912e9e8be0e7fb74df8e200a
onimarufl <moonoimooham@gmail.com>
2018-01-19 11:41:49 +0700

    V1 Succcess!!

---
+++ TechnicianBack/src/main/java/net/kzn/TechnicianBack/App.java
@@ -0,0 +1,13 @@
+package net.kzn.TechnicianBack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
